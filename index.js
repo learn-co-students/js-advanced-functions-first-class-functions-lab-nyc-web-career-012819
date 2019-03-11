aa5b3de4595241ca8256ad971d1bc454750285bd
Matthew Masiello <home@Matthews-MBP.fios-router.home>
2019-03-11 16:41:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(array){
+  return array.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(array){
+  return array.slice(-2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(number){
+  return function(fair){
+    return fair * number;
+  }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function fetchSpecifiedDrivers(array, driversFunc){
+  return driversFunc(array);
+}
